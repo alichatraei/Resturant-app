2e105982cb4ac0b8498c9b816fad67fedf2b041d
Ali chatraei <alichatraei@yahoo.com>
2022-09-10 17:30:49 +0430

    Config axios for api restaurant and call a simple call api

--- src/Screens/ResturantContainer/index.js
+++ src/Screens/ResturantContainer/index.js
@@ -1,5 +1,6 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Text, View } from "react-native";
+import yelp from "../../Api/yelp";
 import SearchBar from "../../Components/SearchBar";
 const reducer = (state, action) => {
   switch (action.type) {
@@ -12,6 +13,7 @@ const reducer = (state, action) => {
   }
 };
 const ResturantContainer = () => {
+  const [result, setResult] = useState([]);
   const [state, dispatch] = useReducer(reducer, {
     searchText: "",
     submitSearchText: "",
@@ -22,6 +24,17 @@ const ResturantContainer = () => {
   const searchOnEndEditingHandler = () => {
     dispatch({ type: "submit_search_text", payload: "Submitted!" });
   };
+  const searchApi = async () => {
+    const resultAPI = await yelp.get("/recipes/complexSearch", {
+      params: {
+        apiKey: "8c2077e5f0f9419ea48c115ce920e0f9",
+      },
+    });
+    setResult(resultAPI.data?.results);
+  };
+  useEffect(() => {
+    searchApi();
+  }, []);
   return (
     <View>
       <SearchBar
@@ -30,6 +43,7 @@ const ResturantContainer = () => {
       />
       <Text> Searched text is : {state.searchText}</Text>
       <Text> Submitted search text is : {state.submitSearchText}</Text>
+      <Text> Amount : {result.length}</Text>
     </View>
   );
 };
