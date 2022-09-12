4d5384cf393263eb490f739fb7c328eeb8d4a2d8
Ali chatraei <alichatraei@yahoo.com>
2022-09-12 12:28:46 +0430

    Change API to yelp

--- src/Api/yelp.js
+++ src/Api/yelp.js
@@ -1,4 +1,8 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://api.spoonacular.com/",
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer OnUQ1NIwpQvcTtAH3rRu4_xtgkOz9ddeBgbPOj6OifyS4mL8v3jkwpk9YgQU6M4KwgA3dU2JBzMBjaaWLP3PGrGlIiffvj-ooh2jCnrHMkc7MJDbqyYOwSL1Ft4eY3Yx",
+  },
 });
